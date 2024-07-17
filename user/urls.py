963481d38aaf376e5696da3f9dfb1a0f7cba07ae
theodiloreto <tdiloreto24@gmail.com>
2024-07-17 16:46:38 +0200

    Initial commit

--- user/urls.py
+++ user/urls.py
@@ -5,12 +5,12 @@ from .views import MyTokenObtainPairView,RegisterUserAPIView
 from django.http import HttpResponse 
 
 router = DefaultRouter()
-router.register(r'', UserViewSet)  
-#router.register(r'user', UserViewSet, basename='user')  
+#router.register(r'', UserViewSet)  
+router.register(r'user', UserViewSet, basename='user')  
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterUserAPIView.as_view(), name='register'),  
-    #path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
 
